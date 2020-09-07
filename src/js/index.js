e1f3b71c31ce983078c04df25e2a4ce9bb37d319
Fabricio <fabricioviazzi@gmail.com>
2020-09-07 00:26:18 -0300

    Do animations after page is rendered
    JS - Wait untile page is fulled load
    View: Add 'loaded' class to body

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
 import './menu.js';
 import { imgLazyLoad, imgbgLazyLoad } from './img-lazy-load.js';
 import './collapse.js';
+
+// Page load event
+window.onload = pageLoaded;
+
+function pageLoaded() {
+	console.log('hey');
+	document.body.classList.add('loaded')
+}
