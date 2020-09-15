89d6cb40e1a9b0bd93701e357f008d9893348a70
Fabricio <fabricioviazzi@gmail.com>
2020-09-15 19:09:40 -0300

    Add leads email

--- upload/system/config.php
+++ upload/system/config.php
@@ -1,15 +1,44 @@
 <?php
 
-  $lang = isset($_COOKIE["language"]) ? $_COOKIE["language"] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+	// Set Language (deprecated for now)
+	$lead = isset($_GET['r']) ? $_GET['r'] : null;
 
-  switch ($lang) {
+	// Make sure there is a lead
+	if ($lead) {
 
-      case "es":
-          require_once 'lang/ES_es.php';
-          break;
+		// Email Template
+		ob_start();
+			include "templates/lead_email.php";
+			$message = ob_get_contents();
+		ob_end_clean();
 
-      default:
-          require_once 'lang/EN_us.php';
-          break;
-  }
+		// Send Email
+		$to       =	'fabricioviazzi@gmail.com';
+		$subject  =	'Form Contact';
+
+		$headers  = "From: Reach <leads@fabsdev.com>" . "\r\n";
+		$headers .= "Reply-To: fabricioviazzi@gmail.com\r\n";
+		$headers .= "MIME-Version: 1.0\r\n";
+		$headers .= "Content-Type: text/html; charset=utf-8\r\n";
+
+		if ( mail($to, $subject, $message, $headers) ) {
+			// All good, do nothing
+		} else {
+			error_log("LEAD EMAIL MISSING, ID WAS: ",$lead);
+		}
+	}
+
+	// Set Language (deprecated for now)
+	$lang = isset($_COOKIE["language"]) ? $_COOKIE["language"] : substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+	switch ($lang) {
+
+			case "es":
+					require_once 'lang/ES_es.php';
+					break;
+
+			default:
+					require_once 'lang/EN_us.php';
+					break;
+	}
 ?>
