1062312c479e24679a9d550816841316e5de9867
Fabricio <fabricioviazzi@gmail.com>
2020-09-17 00:36:23 -0300

    Return falsy value if selector is empty

--- src/js/_selector.js
+++ src/js/_selector.js
@@ -1,4 +1,4 @@
 export default function $(selector) {
 	let nodes = document.querySelectorAll(selector);
-	return nodes.length === 1 ? nodes[0] : nodes;
+	return nodes.length === 1 ? nodes[0] : nodes.length ? nodes : null;
 }
