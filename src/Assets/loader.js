32ed32294370581842dc6125d96f135e2bc055fa
fabricio <fabricioviazzi@gmail.com>
2022-05-25 11:50:32 +0200

    Remove console log call

--- src/Assets/loader.js
+++ src/Assets/loader.js
@@ -3,6 +3,4 @@ const importAll = r => r.keys().map(r)
 const img   = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/))
 const fonts = importAll(require.context('./fonts', false, /\.(ttf|woff|woff2)$/))
 
-console.log(img, fonts)
-
 export { img, fonts }
