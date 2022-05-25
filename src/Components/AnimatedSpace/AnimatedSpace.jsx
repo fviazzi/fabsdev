3bafb6c86f781b67ecf165b682919b2f408e6838
fabricio <fabricioviazzi@gmail.com>
2022-05-25 21:04:28 +0200

    Modularize animated space background

---
+++ src/Components/AnimatedSpace/AnimatedSpace.jsx
@@ -0,0 +1,23 @@
+// External modules
+import React from 'react'
+
+// Internal modules
+import './AnimatedSpace.less'
+
+// Components
+import Space from './Space/Space'
+import EmptySpace from './EmptySpace/EmptySpace'
+
+export default function AnimatedSpace ({ planets, blur }) {
+
+  return (
+    <div
+      id='section-background-container'
+      className={blur ? 'blur' : ''}
+    >
+      {
+        planets ? <Space /> : <EmptySpace />
+      }
+    </div>
+  )
+}
