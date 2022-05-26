c2984c18d90bbcab3aae7c76b6f042612ce3c91b
fabricio <fabricioviazzi@gmail.com>
2022-05-26 23:23:08 +0200

    Add desktop styles for header
    Fix lottie-react double play

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,81 @@
 // External modules
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
+
+// Internal modules
+import './Header.less'
+import { AppContext } from 'Context'
+
+// Components
+import Toggle from './Toggle/Toggle'
 
 export default function Header () {
 
+  // Global state
+  const { state, dispatch } = React.useContext(AppContext)
+
+  // Local state
+  const [mounted, setMounted] = React.useState(false)
+
+  // Constants
+  const links = ['About', 'Skills', 'Work', 'Contact']
+
+  // Mount effect
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  // Methods
+  const toggleTheme = () => {
+
+    // Update global state
+    const theme = state.theme === 'light-theme' ? 'dark-theme' : 'light-theme'
+
+    dispatch({
+      type: 'UPDATE_THEME',
+      data: theme
+    })
+
+    localStorage.setItem('theme', theme)
+  }
+
   return (
-    <header>
-      <nav>
-        <Link to='/home'>Home page</Link>
-        <Link to='/about'>About page</Link>
-      </nav>
-    </header>
+    <CSSTransition
+      timeout={0}
+      classNames='header'
+      unmountOnExit
+      in={mounted}
+    >
+      <header id='main-header'>
+        <nav>
+          <Link
+            to='/'
+            className={state.section === 'intro' ? 'active' : ''}
+          >
+            Home
+          </Link>
+
+          {/* Map links */}
+          {
+            links.map(link => (
+              <Link
+                key={link.toLowerCase()}
+                to={`/${link.toLowerCase()}`}
+                className={state.section === link.toLowerCase() ? 'active' : ''}
+              >
+                {link}
+              </Link>
+            ))
+          }
+        </nav>
+
+        <button
+          onClick={toggleTheme}
+        >
+          <Toggle />
+        </button>
+      </header>
+    </CSSTransition>
   )
 }
---
+++ src/Components/Header/Toggle/Toggle.jsx
@@ -0,0 +1,65 @@
+// External modules
+import React from 'react'
+import Lottie from 'lottie-react'
+
+// Internal modules
+import toggleAnimation from './toggle.json'
+import { AppContext } from 'Context'
+
+export default function Toggle () {
+
+  // Global state
+  const { state } = React.useContext(AppContext)
+
+  const [mounted, setMounted] = React.useState(false)
+  const [loaded, setLoaded]   = React.useState(false)
+
+  // Constants
+  const lottieRef = React.useRef(null)
+
+  // Mount effect
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  // Mount effect
+  React.useEffect(() => {
+
+    lottieRef.current.setSpeed(2)
+    lottieRef.current.stop()
+
+    if (state.theme === 'dark-theme') {
+      lottieRef.current.setDirection(1)
+      lottieRef.current.play()
+    }
+
+  }, [loaded])
+
+  // Toggle effect
+  React.useEffect(() => {
+    if (mounted) {
+
+      // Default functions for dark theme
+      const direction = state.theme === 'light-theme' ? -1 : 1
+
+      lottieRef.current.setDirection(direction)
+
+      toggle()
+    }
+  }, [state.theme])
+
+  const toggle = () => {
+    console.log('mounted')
+    lottieRef.current.play()
+  }
+
+  return (
+    <Lottie
+      lottieRef={lottieRef}
+      animationData={toggleAnimation}
+      loop={false}
+      autoplay={false}
+      onDOMLoaded={() => setLoaded(true)}
+    />
+  )
+}
