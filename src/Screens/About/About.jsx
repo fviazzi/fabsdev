ae770b96cc38406a8aa620a50a579114a151aaca
fabricio <fabricioviazzi@gmail.com>
2022-05-28 22:39:33 +0200

    Add about page

--- src/Screens/About/About.jsx
+++ src/Screens/About/About.jsx
@@ -1,14 +1,96 @@
 // External modules
 import React from 'react'
+import { CSSTransition } from 'react-transition-group'
+import Lottie from 'lottie-react'
 
 // Internal modules
 import './About.less'
+import { AppContext } from 'Context'
+
+// Components
+import AnimatedSpace from 'Components/AnimatedSpace/AnimatedSpace'
+
+// Assets
+import i18n from './about.i18n.json'
+import moon from './moon.json'
+import planet from './planet.json'
+import profile from './profile.png'
+import galaxyShape from './galaxy-shape.png'
 
 export default function About () {
 
+  // Global state
+  const { state, dispatch } = React.useContext(AppContext)
+  const lang = state.language
+  console.log(i18n, lang)
+
+  // Local state
+  const [mounted, setMounted] = React.useState(false)
+
+  // Mount effect
+  React.useEffect(() => {
+
+    dispatch({
+      type: 'UPDATE_SECTION',
+      data: 'about'
+    })
+
+    setMounted(true)
+  }, [])
+
   return (
-    <section id='about-container'>
-      <h1>About page</h1>
-    </section>
+    <CSSTransition
+      timeout={0}
+      classNames='about'
+      unmountOnExit
+      in={mounted}
+    >
+      <section id='about-container'>
+
+        {/* Background */}
+        <AnimatedSpace blur />
+
+        <div className='container'>
+
+          <div className='content'>
+
+            <h1>{i18n[lang].title}</h1>
+
+            {
+              i18n[lang].content.map((content, index) => (
+                <p
+                  key={index}
+                  dangerouslySetInnerHTML={{ __html: content }}
+                />
+              ))
+            }
+          </div>
+
+          <div className='graphics'>
+
+            {/* Galaxy */}
+            <img src={galaxyShape} alt='Shape Background' />
+
+            {/* Profile */}
+            <div className='profile-container'>
+
+              <Lottie
+                animationData={moon}
+                loop
+                autoplay
+              />
+
+              <img src={profile} alt='My Profile picture' />
+
+              <Lottie
+                animationData={planet}
+                loop
+                autoplay
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </CSSTransition>
   )
 }
